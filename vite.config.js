85c974245048084d1dbbc4f0137125bc3982a310
Chris <christopher.d.gibson1@gmail.com>
2026-09-23 16:44:17 +0200

    yml file for GitHub Actions #duo-dash

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/',
-    build: {
-      outDir: 'dist',
-    },
   plugins: [react()],
+  base: '/duopoloy-pricing-dashboard/',
+  build: {
+    outDir: 'dist',
+  },
 });
